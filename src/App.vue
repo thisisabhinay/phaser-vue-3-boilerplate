43751f3021ec5353eeb5efa972102cb196626077
thisisabhinay <a8h1nay@gmail.com>
2024-01-25 17:01:43 +0530

    build: setup vue3 and tailwind

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router"
+</script>
+
+<template>
+  <section class="grid grid-cols-1 gap-10 auto-rows-max max-w-5xl m-auto py-10">
+    <header>
+      <nav class="flex items-center gap-10 text-blue-500">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </header>
+    <h1 class="text-2xl">Phaser.js Boilerplate</h1>
+    <RouterView />
+  </section>
+</template>
+
+<style scoped></style>
